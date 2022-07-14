2d15e93dede108a8a15bdaf8cbc9d51d2be0bf38
Francisco Sanchez-Saez <fcosanchezsaez@gmail.com>
2022-07-14 23:44:36 +0200

    cargar librerías mediante source

---
+++ 0_a_librerias.R
@@ -0,0 +1,25 @@
+# Librerías
+library(readxl)
+library(readr)
+library(data.table)
+library(lubridate)
+library(tidyverse)
+library(knitr)
+library(kableExtra)
+library(plyr)
+library(dplyr)
+library(ggpubr)
+library(scales)
+library(stringr)
+library(plotly)
+library(Matching)
+#library(MatchIt)
+library(plotrix)  # para sacar piramides descriptivas de las tablas_analisis
+library(ggpmisc) # para series temporales
+library(survminer)
+library(survival)
+library(conflicted)
+
+# usar fuciones de tidyverse antes que las de otros paquetes-------------------
+conflict_prefer_all("tidyverse")
+conflict_prefer_all("dplyr")
